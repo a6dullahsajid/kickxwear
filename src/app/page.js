c0511f74d9fd5e20f1c4f2a0959560a431ae701b
Abdullah Sajid <abdullahsajid1029@gmail.com>
2026-06-12 14:09:18 +0530

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 text-amber-700 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      
+      KickXWear
+    </div>
+  );
+}
