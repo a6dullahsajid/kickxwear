06c870b1d937bea45be0648cff795c51e6d29bab
Abdullah Sajid <abdullahsajid1029@gmail.com>
2026-07-19 15:13:44 +0530

    fix: added webmanifest and updated logo

--- src/app/layout.js
+++ src/app/layout.js
@@ -81,13 +81,17 @@ export const metadata = {
   alternates: {
     canonical: "/",
   },
+  manifest: "/site.webmanifest",
   icons: {
     icon: [
       { url: "/favicon.ico", sizes: "any" },
-      { url: "/logo.png", type: "image/png", sizes: "512x512" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+    ],
+    apple: [
+      { url: "/apple-touch-icon.png", sizes: "180x180" },
     ],
     shortcut: "/favicon.ico",
-    apple: "/apple-touch-icon.png",
   },
 
   openGraph: {
